2aa906f54c8bc185ad3d0cafa6e4a9203a173667
Toxic.Co <104733482+ItsToxicGGAlt@users.noreply.github.com>
2022-07-31 19:52:28 +0200

    full revamp (will pay attention into changing this)

--- src/Dctx/Entity/Command/NpcCommand.php
+++ src/Dctx/Entity/Command/NpcCommand.php
@@ -3,46 +3,31 @@
 namespace Dctx\Entity\Command;
 
 use Dctx\Entity\task\NpcTask;
+use Dctx\Entity\Main;
 use Dctx\Entity\Entity\{EntityMain, EntityManager};
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
 class NpcCommand extends Command{
+  
+    public $plugin;
 
-    public function onCommand(CommandSender $sender, Command $cmd, String $label, array $args) : bool {
-		if(strtolower($cmd->getName()) == "nf"){
-			if(!$sender instanceof Player){
-			    $sender->sendMessage("§cPlease use this command In-Game!");
-				return true;
-			}
-			if(isset($args[0])){
-				switch(strtolower($args[0])){
-				    case "help":
-                    if(!$sender->hasPermission("nf.cmd") && !$sender->hasPermission("nf.cmd.help")){
-                        $sender->sendMessage("§cYou do not have permission to use this command!");
-                    return true;
-                }
-                $sender->sendMessage("§aNpc+Form Commands: \n" .
-                    "§7/nf npc : Spawn the Npc\n".
-	            "§7/nf info : SOON\n");
-					break;
-					case "npc":
-					if($sender->hasPermission("nf.npc.cmd")){
-						$npc = new EntityManager();
-						$npc->setMainEntity($sender);
-					} else {
-						$sender->sendMessage("§cYou do not have permission to use this command!");
-                    }
-					break;
-					default:
-					$sender->sendMessage("§cUsage: /nf help");
-					break;
-				}
-			} else {
-				$sender->sendMessage("§cUsage: /nf help");
-			}
-		}
-	return true;
-	} 
-  }
+    public function __construct(Main $plugin)
+    {
+        $this->plugin = $plugin;
+        
+        parent::__construct("mnpc", "§r§fSpawn Ur Own MiniGame NPC, Plugin By ItsToxicGG", "§cUse: /mnpc", ["mnpc"]);
+	$this->setPermission("spawn.mnpc");
+        $this->setAliases(["mn"]);
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+       if($sender instanceof Player){
+           
+       } else {
+           $sender->sendMessage("You not a player");
+       }
+    }	
+}
