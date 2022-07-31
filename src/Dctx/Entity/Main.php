c8a5322065d1e385eea188f50275219a48188d00
Toxic.Co <104733482+ItsToxicGGAlt@users.noreply.github.com>
2022-07-31 19:40:43 +0200

    PM4 Updates + Make it configurable

--- src/Dctx/Entity/Main.php
+++ src/Dctx/Entity/Main.php
@@ -3,17 +3,24 @@
 namespace Dctx\Entity;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\Player;
+use pocketmine\player\Player;;
+use pocketmine\entity\EntityFactory;
+use pocketmine\entity\EntityDataHelper;
+use pocketmine\world\World;
+use pocketmine\nbt\tag\CompoundTag;
 use Dctx\Entity\{Form\Form, Form\FormAPI, Entity\EntityManager, Entity\EntityMain, Task\NpcTask};
 
 class Main extends PluginBase{
 
-	public function onEnable(){
+	public function onEnable(): void{
         $this->getLogger()->info("Plugin Is Enabled!");
-		Entity::registerEntity(EntityMain::class, true);
+	$entityfactory = EntityFactory::getInstance();
+	$entityfactory->register(EntityMain::class, function(World $world, CompoundTag $nbt) :EntityMain{
+		return new EntityMain(EntityDataHelper::parseLocation($nbt, $world), $nbt);
+	}, ['EntityMain']);
     }
 	
-    public function onDisable(){
+    public function onDisable(): void{
         $this->getLogger()->info("Disabling Plugin!");
 	}
 
@@ -21,23 +28,23 @@ class Main extends PluginBase{
 		if ($event->getEntity() instanceof MainEntity) {
 			$player = $event->getDamager();
 			if ($player instanceof Player) {
-				$event->setCancelled(true);
+				$event->cancel();
 				$form = new Form(function (Player $player, int $data = null) {
 					switch($data) {
 					    case 0:
 					    break;
 						case 1:
-						$this->getPlugin()->getServer()->dispatchCommand($player, "say Set Command");
+						$this->getPlugin()->getServer()->dispatchCommand($player, $this->getConfig()->get("Cmd-1"));
 						break;
 						case 2:
-						$this->getPlugin()->getServer()->dispatchCommand($player, "say Set Command");
+						$this->getPlugin()->getServer()->dispatchCommand($player, $this->getConfig()->get("Cmd-2"));
 						break;
 						case 3:
-						$this->getPlugin()->getServer()->dispatchCommand($player, "say Set Command");
+						$this->getPlugin()->getServer()->dispatchCommand($player, $this->getConfig()->get("Cmd-3"));
 						break;
 					}
 				});
-				$form->setTitle("§l§eMINIGAME NAME!");
+				$form->setTitle($this->getConfig()->get("Form-Title"));
 				$form->setContent("§fSelect Mode!");
 				$form->addButton("§l§cEXIT");
 				$form->addButton("§eSOLO");
